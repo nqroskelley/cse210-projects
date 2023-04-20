e783bc51fc0546bf16b13280812d103e76ead6ac
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-04-20 11:23:09 -0600

    Completed C# Prep 2.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int gradePercent = int.Parse(grade);
+        string letter;
+
+        if (gradePercent >= 90)
+        {
+          letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+          letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+          letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+          letter = "D";
+        }
+        else
+        {
+          letter = "F";
+        }
+
+        Console.WriteLine(letter);
+
+        if (gradePercent >= 70)
+        {
+          Console.WriteLine("You passed the course. Great job!");
+        }
+        else
+        {
+          Console.WriteLine("Sorry, you didn't pass the course. Better luck next time!");
+        }
     }
 }
