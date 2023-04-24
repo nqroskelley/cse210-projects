cb38b64aa37d43e47626adfc743571c46a1486f9
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-04-24 10:47:30 -0600

    Added loop.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,22 +7,26 @@ class Program
         Console.Write("What is your magic number? ");
         string numberString = Console.ReadLine();
         int number = int.Parse(numberString);
+        int guess;
 
-        Console.Write("What is your guess? ");
-        string guessString = Console.ReadLine();
-        int guess = int.Parse(guessString);
+        do
+        {
+          Console.Write("What is your guess? ");
+          string guessString = Console.ReadLine();
+          guess = int.Parse(guessString);
 
-        if (guess < number)
-        {
-          Console.WriteLine("Higher");
-        }
-        else if (guess > number)
-        {
-          Console.WriteLine("Lower");
-        }
-        else
-        {
-          Console.WriteLine("You guessed it!");
-        }
+          if (guess < number)
+          {
+            Console.WriteLine("Higher");
+          }
+          else if (guess > number)
+          {
+            Console.WriteLine("Lower");
+          }
+          else
+          {
+            Console.WriteLine("You guessed it!");
+          }
+        } while (guess != number);
     }
 }
