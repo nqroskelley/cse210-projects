a8fb469096e36a90fcf22fb30722679ce8cfa3e5
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-04-24 10:38:04 -0600

    Asks for number and guess, then provides result.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is your magic number? ");
+        string numberString = Console.ReadLine();
+        int number = int.Parse(numberString);
+
+        Console.Write("What is your guess? ");
+        string guessString = Console.ReadLine();
+        int guess = int.Parse(guessString);
+
+        if (guess < number)
+        {
+          Console.WriteLine("Higher");
+        }
+        else if (guess > number)
+        {
+          Console.WriteLine("Lower");
+        }
+        else
+        {
+          Console.WriteLine("You guessed it!");
+        }
     }
 }
