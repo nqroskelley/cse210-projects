431f84cab81bc4986b8170a8616517f3ced19763
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-04-25 11:29:31 -0600

    Gets list, finds sum and average.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finsished.");
+        int input = 1;
+        string numberString;
+        List<int> numbers = new List<int>();
+
+        while (input != 0)
+        {
+          Console.Write("Enter a number: ");
+          numberString = Console.ReadLine();
+          input = int.Parse(numberString);
+          if (input != 0)
+          {
+            numbers.Add(input);
+          }
+        }
+
+        int sum = 0;
+
+        foreach (int number in numbers)
+        {
+          sum = sum + number;
+        }
+
+        String sumString = sum.ToString();
+        Console.WriteLine($"The sum is: {sumString}");
+        int average = sum / numbers.Count;
+        String averageString = average.ToString();
+        Console.WriteLine($"The average is: {averageString}");
     }
 }
