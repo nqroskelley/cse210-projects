945955cefc1bf5481748309d7a6bec482147cafb
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-04-26 11:09:45 -0600

    Welcomes user and asks for name and number.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+          Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+          Console.Write("Please enter your name: ");
+          string name = Console.ReadLine();
+          return name;
+        }
+
+        static int PromptUserNumber()
+        {
+          Console.Write("Please enter your favorite number: ");
+          string numberString = Console.ReadLine();
+          int number = int.Parse(numberString);
+          return number;
+        }
+
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
     }
 }
