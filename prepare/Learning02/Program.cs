5871f13d0536bde8770690939d7356a2eb45b537
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-05-02 11:04:40 -0600

    Changed values for year variables.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,14 +7,14 @@ class Program
         Job job1 = new Job();
         job1._company = "Applebee's";
         job1._jobTitle = "Waiter";
-        job1._startYear = 2021;
-        job1._endYear = 2023;
+        job1._startYear = 2008;
+        job1._endYear = 2016;
 
         Job job2 = new Job();
         job2._company = "Walmart";
         job2._jobTitle = "Cashier";
-        job2._startYear = 2008;
-        job2._endYear = 2013;
+        job2._startYear = 2017;
+        job2._endYear = 2023;
 
         Resume resume = new Resume();
         resume._jobs.Add(job1);
