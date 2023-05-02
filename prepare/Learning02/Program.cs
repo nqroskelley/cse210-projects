101b26503c02f3d9d61c0accb3aabaa39e0004ed
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-05-02 10:13:16 -0600

    Added two job instances and set  member variables.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Applebee's";
+        job1._jobTitle = "Waiter";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Walmart";
+        job2._jobTitle = "Cashier";
+        job2._startYear = 2008;
+        job2._endYear = 2013;
+
+        Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);
     }
 }
