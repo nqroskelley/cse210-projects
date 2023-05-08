5df93c9ad06c5180586f1a4f09f8d040c3e765f1
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-05-08 10:31:06 -0600

    Implemented menu, exit, and invalid input message.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        bool finished = false;
+        string choice = "";
+        Console.WriteLine("Welcome to the Journal Program!");
+
+        while (finished != true)
+        {
+          Console.WriteLine("Please enter the number corresponding to the option you wish to select:");
+          Console.WriteLine("1. Write in Journal");
+          Console.WriteLine("2. Display Journal");
+          Console.WriteLine("3. Load Journal");
+          Console.WriteLine("4. Save Journal");
+          Console.WriteLine("5. Exit");
+          Console.Write("What would you like to do? ");
+          choice = Console.ReadLine();
+          if (choice == "1")
+          {}
+          else if (choice == "2")
+          {}
+          else if (choice == "3")
+          {}
+          else if (choice == "4")
+          {}
+          else if (choice == "5")
+          {
+            Console.Write("Goodbye!");
+            finished = true;
+          }
+          else
+          {
+            Console.WriteLine("Sorry, I didn't understand that.");
+          }
+        }
     }
 }
