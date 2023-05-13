e4ca373a5578bd3345c659c969937dd6cec1e32d
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-05-13 11:48:33 -0600

    Can now cancel options and ask for new prompts.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,8 @@
 using System;
-
+//The following features have been implemented to exceed requirements:
+//The ability to cancel options 1, 3, and 4. (Type 'c')
+//The ability to ask for a new prompt in option 1. (Type 'p')
+//Let's the user know when an invalid command is entered at the menu.
 class Program
 {
     static void Main(string[] args)
@@ -24,13 +27,22 @@ class Program
           {
             Entry entry = new Entry();
             Prompt prompt = new Prompt();
-            prompt.GetPrompt();
-            entry._prompt = prompt._prompt;
-            Console.Write("> ");
-            entry._entry = Console.ReadLine();
-            DateTime dateTime = DateTime.Now;
-            entry._date = dateTime.ToShortDateString();
-            _entries.Add(entry);
+            do
+            {
+              prompt.GetPrompt();
+              Console.WriteLine("Type 'c' to cancel, or type 'p' to ask for a new prompt (it may take multiple attempts).");
+              Console.Write("> ");
+              entry._entry = Console.ReadLine();
+              if (entry._entry != "c" && entry._entry != "p")
+              {
+                entry._prompt = prompt._prompt;
+                DateTime dateTime = DateTime.Now;
+                entry._date = dateTime.ToShortDateString();
+                _entries.Add(entry);
+              }
+              else
+              {}
+            } while (entry._entry == "p");
           }
           else if (choice == "2")
           {
@@ -42,30 +54,37 @@ class Program
           else if (choice == "3")
           {
             Console.WriteLine("Please enter the filename.");
+            Console.WriteLine("Type 'c' to cancel.");
             filename = Console.ReadLine();
-            string[] lines = System.IO.File.ReadAllLines(filename);
-            _entries.Clear();
-
-            foreach(string line in lines)
+            if (filename != "c")
             {
-              string[] variables = line.Split("|");
-              Entry entry = new Entry();
-              entry._date = variables[0];
-              entry._prompt = variables[1];
-              entry._entry = variables[2];
-              _entries.Add(entry);
+              string[] lines = System.IO.File.ReadAllLines(filename);
+              _entries.Clear();
+
+              foreach(string line in lines)
+              {
+                string[] variables = line.Split("|");
+                Entry entry = new Entry();
+                entry._date = variables[0];
+                entry._prompt = variables[1];
+                entry._entry = variables[2];
+                _entries.Add(entry);
+              }
             }
           }
           else if (choice == "4")
           {
             Console.WriteLine("Please name the file.");
+            Console.WriteLine("Type 'c' to cancel.");
             filename = Console.ReadLine();
-
-            using (StreamWriter file = new StreamWriter(filename))
+            if (filename != "c")
             {
-              foreach(Entry entry in _entries)
+              using (StreamWriter file = new StreamWriter(filename))
               {
-                file.WriteLine($"{entry._date}|{entry._prompt}|{entry._entry}");
+                foreach(Entry entry in _entries)
+                {
+                  file.WriteLine($"{entry._date}|{entry._prompt}|{entry._entry}");
+                }
               }
             }
           }
