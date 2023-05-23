350022dd2d6f9e63d1007f64ed5e164b93988670
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-05-23 11:22:22 -0600

    Added constructor and more variables to Scripture.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         string _input = "";
         Reference _reference = new Reference("James", 1, 5, 6);
-        Scripture _scripture = new Scripture();
+        Scripture _scripture = new Scripture(_reference.GetReference(), "If any of you lack wisdom, let him ask of God, that giveth to all men liberally, and upbraideth not; and it shall be given him. But let him ask in faith, nothing wavering. For he that wavereth is like a wave of the sea driven with the wind and tossed.");
 
         while (_input != "quit")
         {
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -4,4 +4,18 @@ class Scripture
 {
   private List<Word> _words = new List<Word>();
   private string _reference;
+  private string[] _splitVerse;
+  private Word _word;
+
+  public Scripture(string reference, string verse)
+  {
+    _reference = reference;
+    _splitVerse = verse.Split(' ');
+
+    foreach(string word in _splitVerse)
+    {
+      _word = new Word(word);
+      _words.Add(_word);
+    }
+  }
 }
