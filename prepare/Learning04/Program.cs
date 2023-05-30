239ed67809541154cbcd5758605a7809b8d84ee6
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-05-30 10:16:17 -0600

    Created MathAssignment class.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,6 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
-        Console.Write(assignment.getSummary());
+        Console.WriteLine(assignment.getSummary());
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.getSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
     }
 }
