e80676aaaf2afc7b5077b5a4cfdb0d4688eade7d
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-05-30 10:28:09 -0600

    Created WritingAssignement class.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,8 +6,13 @@ class Program
     {
         Assignment assignment = new Assignment("Samuel Bennet", "Multiplication");
         Console.WriteLine(assignment.getSummary());
+
         MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssignment.getSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "Europeon History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.getSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
