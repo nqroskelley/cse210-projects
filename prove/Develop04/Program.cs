29f6427ca9e3b1a204794388eb9c7a409c37e21b
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-06-05 10:29:05 -0600

    Created _choice variable and main menu loop.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string _choice = "";
+
+        Console.WriteLine("Welcome to the Mindfulness Program!");
+
+        while (_choice != "4")
+        {
+          Console.WriteLine("Please select which activity you would like to do:");
+          Console.WriteLine("1. Breathing Activity");
+          Console.WriteLine("2. Reflection Activity");
+          Console.WriteLine("3. Listing Activity");
+          Console.WriteLine("4. Quit");
+
+          _choice = Console.ReadLine();
+
+          if (_choice == "1")
+          {}
+          else if (_choice == "2")
+          {}
+          else if (_choice == "3")
+          {}
+          else if (_choice == "4")
+          {
+            Console.WriteLine("Goodbye!");
+          }
+          else
+          {
+            Console.WriteLine("Sorry, I didn't understand that.");
+          }
+        }
     }
 }
