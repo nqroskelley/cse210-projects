77a217c883551c83ccf967274a6319d23463f973
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-06-05 10:35:29 -0600

    Created member variables for Activity class.

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -1,4 +1,12 @@
 using System;
 
 class Activity
-{}
+{
+  private int _time;
+  private int _countdown;
+  private DateTime _start;
+  private DateTime _stop;
+  private string _activity;
+  private string _description;
+  private Random _random;
+}
