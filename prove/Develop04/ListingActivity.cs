5e574a7c998ee0f524cd272db4b5dd8d66ef55db
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-06-05 10:40:20 -0600

    Created member variables for child classes.

--- prove/Develop04/ListingActivity.cs
+++ prove/Develop04/ListingActivity.cs
@@ -1,4 +1,7 @@
 using System;
 
 class ListingActivity : Activity
-{}
+{
+  private List<string> _questions;
+  private int _answers;
+}
--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -1,4 +1,7 @@
 using System;
 
 class ReflectionActivity : Activity
-{}
+{
+  private List<string> _prompts;
+  private List<string> _questions;
+}
