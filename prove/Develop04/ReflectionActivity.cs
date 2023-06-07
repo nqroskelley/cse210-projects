4b672d6f95e83343e8e8698272ebb2b8ddec085e
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-06-07 11:29:46 -0600

    Added prompts and questions in ReflectionActivity.

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -7,5 +7,18 @@ class ReflectionActivity : Activity
   public ReflectionActivity (string activity, string description) : base (activity, description)
   {
     _prompts = new List<string>();
+    _prompts.Add("Think of a time when you chose to do the right thing, even though it was hard.");
+    _prompts.Add("Think of a time when you overcame a difficult trial.");
+    _prompts.Add("Think of a time when you comforted someone.");
+    _prompts.Add("Think of a time when you willingly gave service to someone.");
+    GetQuestions().Add("What did you learn from this experience?");
+    GetQuestions().Add("How did this experience make you feel?");
+    GetQuestions().Add("If something like this happens again, what will you do differently?");
+    GetQuestions().Add("What other situations could having this experience be helpful with?");
+    GetQuestions().Add("What was something that stood out to you about this experience?");
+    GetQuestions().Add("How did this experience begin?");
+    GetQuestions().Add("How did this experience end?");
+    GetQuestions().Add("How did this experience affect you personally?");
+    GetQuestions().Add("What is something that you remember fondly about this experience?");
   }
 }
