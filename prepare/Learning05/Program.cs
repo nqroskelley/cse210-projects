ef6a9d1e608175c8436b51051c3e723c54f01917
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-06-13 10:48:23 -0600

    Created Square class.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 5);
+
+        Console.WriteLine(square.GetColor());
+        Console.WriteLine(square.GetArea());
     }
 }
