063062b0a39ff126df43ba69197594c24ab09aaf
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-06-13 11:00:48 -0600

    Created Rectangle class and Circle class.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,8 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Square square = new Square("Red", 5);
+        Rectangle rectangle = new Rectangle("Yellow", 3, 2);
+        Circle circle = new Circle("Blue", 4);
 
         Console.WriteLine(square.GetColor());
         Console.WriteLine(square.GetArea());
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
     }
 }
