a6b6ce4556eaf9d6661bd9dceabd6ddfe2e40ede
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-06-13 11:08:35 -0600

    Creates and iterates through list of shapes.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -8,11 +8,15 @@ class Program
         Rectangle rectangle = new Rectangle("Yellow", 3, 2);
         Circle circle = new Circle("Blue", 4);
 
-        Console.WriteLine(square.GetColor());
-        Console.WriteLine(square.GetArea());
-        Console.WriteLine(rectangle.GetColor());
-        Console.WriteLine(rectangle.GetArea());
-        Console.WriteLine(circle.GetColor());
-        Console.WriteLine(circle.GetArea());
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+          Console.WriteLine(shape.GetColor());
+          Console.WriteLine(shape.GetArea());
+        }
     }
 }
