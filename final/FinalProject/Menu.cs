c232721a56e0b3f6f221faa1d8eea11b8ce2c4aa
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-07-03 10:07:57 -0600

    Added classes to Menu and if statement to Program.

--- final/FinalProject/Menu.cs
+++ final/FinalProject/Menu.cs
@@ -5,5 +5,17 @@ class Menu
   public void UnitMenu ()
   {
     Console.WriteLine("What kind of unit unit do you want to convert?");
+    Console.WriteLine("1. Length");
+    Console.WriteLine("2. Mass");
+    Console.WriteLine("3. Time");
   }
+
+  public void LengthMenu ()
+  {}
+
+  public void MassMenu ()
+  {}
+
+  public void TimeMenue ()
+  {}
 }
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -12,9 +12,33 @@ class Program
 
         while (_input != "quit" && _input != "Quit")
         {
+          Console.WriteLine("");
+
           _menu.UnitMenu();
 
           _input = Console.ReadLine();
+
+          if (_input == "1")
+          {
+            _menu.LengthMenu();
+          }
+          else if (_input == "2")
+          {
+            _menu.MassMenu();
+          }
+          else if (_input == "3")
+          {
+            _menu.TimeMenue();
+          }
+          else if (_input == "quit" || _input == "Quit")
+          {
+            Console.WriteLine("Goodbye!");
+          }
+          else
+          {
+            Console.Clear();
+            Console.WriteLine("Sorry, I didn't understand that.");
+          }
         }
     }
 }
