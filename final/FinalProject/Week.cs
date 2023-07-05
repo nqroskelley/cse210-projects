22d35f75dc30330a1daf67d477e9e6d865d5313c
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-07-05 10:39:42 -0600

    Created child classes for Time.

---
+++ final/FinalProject/Week.cs
@@ -0,0 +1,42 @@
+using System;
+
+class Week : Time
+{
+  public Week (string unit, double amount) : base (unit, amount)
+  {}
+
+  public override double ToSeconds()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToMinutes()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToHours()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToDays()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToWeeks()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToMonths()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToYears()
+  {
+    throw new NotImplementedException();
+  }
+}
