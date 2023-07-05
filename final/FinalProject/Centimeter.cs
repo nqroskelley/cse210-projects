a423d534895f2e5c0635114afe13461fd6dee87f
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-07-05 10:26:02 -0600

    Created child classes for Length.

---
+++ final/FinalProject/Centimeter.cs
@@ -0,0 +1,47 @@
+using System;
+
+class Centimeter : Length
+{
+  public Centimeter (string unit, double amount) : base (unit, amount)
+  {}
+
+  public override double ToInches()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToFeet()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToYards()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToMiles()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToMillimeters()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToCentimeters()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToMeters()
+  {
+    throw new NotImplementedException();
+  }
+
+  public override double ToKilometers()
+  {
+    throw new NotImplementedException();
+  }
+}
