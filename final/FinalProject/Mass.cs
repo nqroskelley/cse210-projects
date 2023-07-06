9508bb47b06ab53e2b9c3aa92815275c9dcc10f8
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-07-06 10:19:50 -0600

    Mass class complete.

--- final/FinalProject/Mass.cs
+++ final/FinalProject/Mass.cs
@@ -3,7 +3,38 @@ using System;
 abstract class Mass : Unit
 {
   public Mass (string unit, double amount) : base (unit, amount)
-  {}
+  {
+    if (unit == "1")
+    {
+      SetUnit2("ounce");
+      SetResult(ToOunces());
+    }
+    else if (unit == "2")
+    {
+      SetUnit2("pound");
+      SetResult(ToPounds());
+    }
+    else if (unit == "3")
+    {
+      SetUnit2("ton");
+      SetResult(ToTons());
+    }
+    else if (unit == "4")
+    {
+      SetUnit2("milligram");
+      SetResult(ToMilligrams());
+    }
+    else if (unit == "5")
+    {
+      SetUnit2("gram");
+      SetResult(ToGrams());
+    }
+    else if (unit == "6")
+    {
+      SetUnit2("kilogram");
+      SetResult(ToKilograms());
+    }
+  }
 
   public abstract double ToOunces ();
 
