b4a17477ba5e4a1d9859e49d7db8a139cb206f68
nqroskelley <72409287+nqroskelley@users.noreply.github.com>
2023-07-06 12:31:03 -0600

    Fixed typo.

--- final/FinalProject/Menu.cs
+++ final/FinalProject/Menu.cs
@@ -6,7 +6,7 @@ class Menu
 
   public string UnitMenu ()
   {
-    Console.WriteLine("What kind of unit unit do you want to convert?");
+    Console.WriteLine("What kind of unit do you want to convert?");
     Console.WriteLine("1. Length");
     Console.WriteLine("2. Mass");
     Console.WriteLine("3. Time");
